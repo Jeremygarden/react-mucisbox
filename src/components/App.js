0403dd036c8e9ed9c431638ecf1de7bd7fba0003
Kay Qiu <yokoyau159@gmail.com>
2019-07-04 17:46:51 -0400

    Add react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,50 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+  NavLink
+} from "react-router-dom";
+import Recommend from "./recommend/Recommend";
+import Ranking from "./ranking/Ranking";
+import Search from "./search/Search";
 import "../assets/scss/App.scss";
 
 function App() {
   return (
-    <div className="app">
-      <header className="app-header">
-        <h1>React Music App</h1>
-      </header>
-      <div className="tab-nav">
-        <div className="tab-item selected">
-          <span>Made For You</span>
+    <Router>
+      <div className="app">
+        <div className="tab-nav">
+          <div className="tab-item selected">
+            <NavLink to="/recommend" className="nav-link">
+              <span>Made For You</span>
+            </NavLink>
+          </div>
+          <div className="tab-item">
+            <NavLink to="/ranking" className="nav-link">
+              <span>Ranking</span>
+            </NavLink>
+          </div>
+          <div className="tab-item">
+            <NavLink to="/search" className="nav-link">
+              <span>Search</span>
+            </NavLink>
+          </div>
         </div>
-        <div className="tab-item">
-          <span>Ranking</span>
-        </div>
-        <div className="tab-item">
-          <span>Search</span>
+        <div className="music-panel">
+          <header className="app-header">
+            <h1>React Music App</h1>
+          </header>
+          <Switch>
+            <Route path="/recommend" component={Recommend} />
+            <Route path="/ranking" component={Ranking} />
+            <Route path="/search" component={Search} />
+            <Route component={Recommend} />
+          </Switch>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
