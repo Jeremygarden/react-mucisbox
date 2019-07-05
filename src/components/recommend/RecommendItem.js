b797587cfcea956ee0abd6bd253c2455835b984d
Kay Qiu <yokoyau159@gmail.com>
2019-07-05 16:02:09 -0400

    adding ranking layout (#5)
    * adjust tab nav active style
    * Theme ranking page

--- src/components/recommend/RecommendItem.js
+++ src/components/recommend/RecommendItem.js
@@ -3,7 +3,11 @@ import React, { Component } from "react";
 class RecommendItem extends Component {
   render() {
     const { name } = this.props;
-    return <li className="list-item">{name}</li>;
+    return (
+      <li className="list-item">
+        <div className="list-item--wrapper">{name}</div>
+      </li>
+    );
   }
 }
 
